a5707718c07f499cd45b7ca0b9db3f292daff9d0
JuanKmilo03 <juancamilobame@ufps.edu.co>
2024-09-27 19:18:36 -0500

    feat(products): create credit and payment models

--- products/models.py
+++ products/models.py
@@ -64,3 +64,42 @@ class Product(models.Model):
 
 # CREDIT MODEL
 
+class Credit(models.Model):
+    # this is not necesary the error is for the linter
+    # id = models.AutoField(primary_key=True)
+    STATUS_CHOICES = [
+        ('started', 'Started'),
+        ('active', 'Active'),
+        ('completed', 'Completed'),
+    ]
+    client = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={
+                               'role': 'client'})  # Only clients can have credits
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    # def _str_(self):
+    #     return f"Credit {self.pk} for {self.client.username}"
+    def str(self):
+        return f"Credit {self.id} for {self.client.username}"
+
+# PAYMENT MODEL
+
+
+class Payment(models.Model):
+    # this is not necesary the error is for the linter
+    id = models.AutoField(primary_key=True)
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('delayed', 'Delayed'),
+    ]
+    credit = models.ForeignKey(Credit, on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    due_in = models.DateField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    # def _str_(self):
+    #     return f"Payment {self.pk} for Credit {self.credit.pk}"
+    def str(self):
+        return f"Payment {self.id} for Credit {self.credit.id}"
